77ecf36045af86589290aac9c16b2dd0aed82409
Morathii <morathii@icloud.com>
2025-04-10 13:29:30 +0200

    :wrench: Support commits with external data folder

--- scripts/git-autocommit.sh
+++ scripts/git-autocommit.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "$0")/.." || exit 1
+source scripts/shared.sh
+
+get_data_folder DATA_FOLDER
+MAKE_NOTES_ROOT="$(dirname "$0")/.."
+cd "${DATA_FOLDER:-$MAKE_NOTES_ROOT}" || exit 1
 
 MESSAGE="${1:-:memo: Auto-commit on $(date '+%Y-%m-%d %H:%M:%S')}"
 git add .
