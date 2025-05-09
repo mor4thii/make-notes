ba3aac71c7bcf9b2a2bf9dd333b2ba7d5275b6a3
Morathii <morathii@icloud.com>
2025-05-09 16:08:48 +0200

    :wrench: Update sync script for XDG based directories

--- scripts/sync-tasks.sh
+++ scripts/sync-tasks.sh
@@ -4,10 +4,10 @@ source scripts/shared.sh
 
 get_data_folder DATA_FOLDER
 
-CONFIG_TARGET="${DATA_FOLDER:-.}/backups"
+CONFIG_TARGET="${DATA_FOLDER:-.}/backups/.config/task"
 mkdir -p "$CONFIG_TARGET"
-rsync -av --delete ~/.taskrc "$CONFIG_TARGET/.taskrc"
+rsync -av --delete ~/.config/task/taskrc "$CONFIG_TARGET/taskrc"
 
-DATA_TARGET="${DATA_FOLDER:-.}/backups/.task/"
+DATA_TARGET="${DATA_FOLDER:-.}/backups/.local/share/task"
 mkdir -p "$DATA_TARGET"
-rsync -av --delete ~/.task/ "$DATA_TARGET"
+rsync -av --delete ~/.local/share/task/ "$DATA_TARGET"
